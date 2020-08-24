67976ef402185a4438f116989e96910fb58c2600
seol <seol@DESKTOP-5EJ2UK9>
2020-08-24 10:41:20 +0900

    초기 커밋.

---
+++ BarCode_Print.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace BarCode
+{
+    partial class BarCode_Print
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BarCode_Print));
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.bar1 = new DevExpress.XtraBars.Bar();
+            this.btn_ExcelUp = new DevExpress.XtraBars.BarButtonItem();
+            this.btn_Print = new DevExpress.XtraBars.BarButtonItem();
+            this.btn_Reset = new DevExpress.XtraBars.BarButtonItem();
+            this.btn_Exit = new DevExpress.XtraBars.BarButtonItem();
+            this.bar3 = new DevExpress.XtraBars.Bar();
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.repositoryItemCheckEdit2 = new DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit();
+            this.Barcode = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.PT = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemCheckEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit();
+            this.behaviorManager1 = new DevExpress.Utils.Behaviors.BehaviorManager(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // barManager1
+            // 
+            this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
+            this.bar1,
+            this.bar3});
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.btn_ExcelUp,
+            this.btn_Print,
+            this.barButtonItem1,
+            this.btn_Exit,
+            this.btn_Reset});
+            this.barManager1.MaxItemId = 7;
+            this.barManager1.StatusBar = this.bar3;
+            // 
+            // bar1
+            // 
+            this.bar1.BarName = "Tools";
+            this.bar1.DockCol = 0;
+            this.bar1.DockRow = 0;
+            this.bar1.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+            this.bar1.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.btn_ExcelUp),
+            new DevExpress.XtraBars.LinkPersistInfo(this.btn_Print),
+            new DevExpress.XtraBars.LinkPersistInfo(this.btn_Reset),
+            new DevExpress.XtraBars.LinkPersistInfo(this.btn_Exit)});
+            this.bar1.Text = "Tools";
+            // 
+            // btn_ExcelUp
+            // 
+            this.btn_ExcelUp.Caption = "엑셀업로드";
+            this.btn_ExcelUp.Id = 0;
+            this.btn_ExcelUp.ImageOptions.Image = global::BarCode.Properties.Resources.exporttoxlsx_32x32;
+            this.btn_ExcelUp.ImageOptions.LargeImage = global::BarCode.Properties.Resources.exporttoxlsx_32x32;
+            this.btn_ExcelUp.Name = "btn_ExcelUp";
+            this.btn_ExcelUp.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph;
+            this.btn_ExcelUp.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btn_ExcelUp_ItemClick);
+            // 
+            // btn_Print
+            // 
+            this.btn_Print.Caption = "바코드출력";
+            this.btn_Print.Id = 1;
+            this.btn_Print.ImageOptions.Image = global::BarCode.Properties.Resources.barcode2_32x32;
+            this.btn_Print.ImageOptions.LargeImage = global::BarCode.Properties.Resources.barcode2_32x32;
+            this.btn_Print.Name = "btn_Print";
+            this.btn_Print.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph;
+            this.btn_Print.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btn_Print_ItemClick);
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Caption = "목록지우기";
+            this.btn_Reset.Id = 6;
+            this.btn_Reset.ImageOptions.Image = global::BarCode.Properties.Resources.fitnone_32x32;
+            this.btn_Reset.ImageOptions.LargeImage = global::BarCode.Properties.Resources.fitnone_32x32;
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph;
+            this.btn_Reset.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btn_Reset_ItemClick);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Caption = "종료";
+            this.btn_Exit.Id = 5;
+            this.btn_Exit.ImageOptions.Image = global::BarCode.Properties.Resources.close_32x32;
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph;
+            this.btn_Exit.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btn_Exit_ItemClick);
+            // 
+            // bar3
+            // 
+            this.bar3.BarName = "Status bar";
+            this.bar3.CanDockStyle = DevExpress.XtraBars.BarCanDockStyle.Bottom;
+            this.bar3.DockCol = 0;
+            this.bar3.DockRow = 0;
+            this.bar3.DockStyle = DevExpress.XtraBars.BarDockStyle.Bottom;
+            this.bar3.OptionsBar.AllowQuickCustomization = false;
+            this.bar3.OptionsBar.DrawDragBorder = false;
+            this.bar3.OptionsBar.UseWholeRow = true;
+            this.bar3.Text = "Status bar";
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Manager = this.barManager1;
+            this.barDockControlTop.Size = new System.Drawing.Size(884, 40);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 741);
+            this.barDockControlBottom.Manager = this.barManager1;
+            this.barDockControlBottom.Size = new System.Drawing.Size(884, 20);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 40);
+            this.barDockControlLeft.Manager = this.barManager1;
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 701);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(884, 40);
+            this.barDockControlRight.Manager = this.barManager1;
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 701);
+            // 
+            // barButtonItem1
+            // 
+            this.barButtonItem1.Caption = "종료";
+            this.barButtonItem1.Id = 3;
+            this.barButtonItem1.ImageOptions.Image = global::BarCode.Properties.Resources.close_32x32;
+            this.barButtonItem1.Name = "barButtonItem1";
+            this.barButtonItem1.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph;
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 40);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.MenuManager = this.barManager1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemCheckEdit1,
+            this.repositoryItemCheckEdit2});
+            this.gridControl1.Size = new System.Drawing.Size(884, 701);
+            this.gridControl1.TabIndex = 4;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.Barcode,
+            this.PT});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsSelection.MultiSelect = true;
+            this.gridView1.OptionsSelection.MultiSelectMode = DevExpress.XtraGrid.Views.Grid.GridMultiSelectMode.CheckBoxRowSelect;
+            this.gridView1.OptionsSelection.ShowCheckBoxSelectorInColumnHeader = DevExpress.Utils.DefaultBoolean.True;
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            this.gridView1.RowCellStyle += new DevExpress.XtraGrid.Views.Grid.RowCellStyleEventHandler(this.gridView1_RowCellStyle);
+            // 
+            // repositoryItemCheckEdit2
+            // 
+            this.repositoryItemCheckEdit2.AutoHeight = false;
+            this.repositoryItemCheckEdit2.Name = "repositoryItemCheckEdit2";
+            this.repositoryItemCheckEdit2.NullStyle = DevExpress.XtraEditors.Controls.StyleIndeterminate.Unchecked;
+            this.repositoryItemCheckEdit2.ValueChecked = "1";
+            this.repositoryItemCheckEdit2.ValueUnchecked = "0";
+            // 
+            // Barcode
+            // 
+            this.Barcode.Caption = "바코드";
+            this.Barcode.FieldName = "Barcode";
+            this.Barcode.Name = "Barcode";
+            this.Barcode.Visible = true;
+            this.Barcode.VisibleIndex = 1;
+            // 
+            // PT
+            // 
+            this.PT.Caption = "P/T";
+            this.PT.FieldName = "PT";
+            this.PT.Name = "PT";
+            this.PT.Visible = true;
+            this.PT.VisibleIndex = 2;
+            // 
+            // repositoryItemCheckEdit1
+            // 
+            this.repositoryItemCheckEdit1.AutoHeight = false;
+            this.repositoryItemCheckEdit1.Name = "repositoryItemCheckEdit1";
+            // 
+            // BarCode_Print
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 761);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BarCode_Print";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "바코드 출력";
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.Bar bar1;
+        private DevExpress.XtraBars.Bar bar3;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.XtraBars.BarButtonItem btn_ExcelUp;
+        private DevExpress.XtraBars.BarButtonItem btn_Print;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+        private DevExpress.XtraBars.BarButtonItem btn_Exit;
+        private DevExpress.XtraBars.BarButtonItem btn_Reset;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit repositoryItemCheckEdit1;
+        private DevExpress.Utils.Behaviors.BehaviorManager behaviorManager1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit repositoryItemCheckEdit2;
+        private DevExpress.XtraGrid.Columns.GridColumn Barcode;
+        private DevExpress.XtraGrid.Columns.GridColumn PT;
+    }
+}
+
---
+++ BarCode_Print.cs
@@ -0,0 +1,215 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraGrid.Extensions;
+using DevExpress.XtraGrid.Views.Grid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarCode
+{
+    public partial class BarCode_Print : Form
+    {
+        public BarCode_Print()
+        {
+            InitializeComponent();
+            GridSetting();
+        }
+
+        private void btn_ExcelUp_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            if (gridView1.RowCount > 0)
+            {
+                MessageBox.Show("이미 업로드한 정보가 있습니다.", "오류");
+            }
+            else
+            {
+                OpenFileDialog openFile = new OpenFileDialog();
+                openFile.Filter = "엑셀파일(*.xlsx)|*.xlsx|(*.xls)|*.xls";
+                openFile.ShowDialog();
+                gridControl1.DataSource = GetSheet(openFile.FileName);
+            }
+        }
+
+        private DataTable GetSheet(string FilePath)
+        {
+            string oledbConnectionString = string.Empty;
+            DataTable dt_Result = null;
+            DataTable dt = null;
+            string sheetName = string.Empty;
+            string sQuery = string.Empty;
+            OleDbDataAdapter adt = null;
+
+            try
+            {
+                if (FilePath.IndexOf(".xlsx") > -1)
+                {
+                    oledbConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;dATA sOURCE=" + FilePath + ";Extended Properties=\"Excel 12.0;\"";
+
+                    using (OleDbConnection con = new OleDbConnection(oledbConnectionString))
+                    {
+                        con.Open();
+
+                        dt = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                        sheetName = dt.Rows[0]["TABLE_NAME"].ToString();
+                        sQuery = string.Format(" SELECT * FROM [{0}]", sheetName);
+
+                        dt_Result = new DataTable();
+                        adt = new OleDbDataAdapter(sQuery, con);
+                        adt.Fill(dt_Result);
+                    }
+                }
+                else if (FilePath.IndexOf(".xls") > -1)
+                {
+                    oledbConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;DataSource=" + FilePath + ";Extended Properties=\"Excel 8.0;\"";
+
+                    using (OleDbConnection con = new OleDbConnection(oledbConnectionString))
+                    {
+                        con.Open();
+
+                        dt = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                        sheetName = dt.Rows[0]["TABLE_NAME"].ToString();
+                        sQuery = string.Format(" SELECT * FROM [{0}]", sheetName);
+
+                        dt_Result = new DataTable();
+                        adt = new OleDbDataAdapter(sQuery, con);
+                        adt.Fill(dt_Result);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("확장자 .xls, .xlsx 만 선택 가능합니다.", "확인");
+                }
+
+                return dt_Result;
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message, "확인");
+                return dt_Result;
+            }
+        }
+
+        private void btn_Print_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            CheckEdit check = sender as CheckEdit;
+
+            if(gridView1.SelectedRowsCount == 0)
+            {
+                MessageBox.Show("목록에서 출력 정보를 선택하세요.");
+                return;
+            }
+
+            string defaultPrinter = string.Empty;
+            string barCode = string.Empty;
+            string pt = string.Empty;
+            
+            PrinterSettings ps = new PrinterSettings();
+            foreach (string printer in PrinterSettings.InstalledPrinters)
+            {
+                ps.PrinterName = printer;
+                if (ps.IsDefaultPrinter)
+                {
+                    defaultPrinter = printer;
+                }
+            }
+
+            var rows = gridView1.GetSelectedRows();
+            foreach(int row in rows)
+            {
+                barCode = gridView1.GetRowCellValue(row, "Barcode").ToString();
+                pt = gridView1.GetRowCellValue(row, "PT").ToString();
+
+                PrintBarCode(defaultPrinter, barCode, pt);
+            }
+
+        }
+
+        private void PrintBarCode(string printer, string barCode, string pt)
+        {
+            string xLocation = string.Empty;
+
+            if(pt.Length == 1) { xLocation = "120"; }
+            else if(pt.Length == 2) { xLocation = "90"; }
+            else if(pt.Length == 3) { xLocation = "65"; }
+
+            PrintDialog pd = new PrintDialog();
+
+            string s = "";
+
+            s += "^XA";
+
+            // PT 테두리(원)
+            s += "^FO40,100^GC220,3,B^FS";
+
+            // PT
+            s += "^FO" + xLocation + ",165^A0N,120,120^FD" + pt + "^FS";
+
+            // 128바코드
+            s += "^FO305,100^BY2^BCN,120,N,N^FD" + barCode + "^FS";
+
+            // 바코드 값
+            s += "^FO350,270^A0N,40,40^FD" + barCode + "^FS";
+        
+            s += "^XZ";
+
+            CM_Function.RawPrinterHelper.SendStringToPrinter(printer, s);
+        }
+
+        private void btn_Exit_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_Reset_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            if(gridView1.RowCount == 0)
+            {
+                MessageBox.Show("목록이 비어 있습니다.", "오류");
+                return;
+            }
+            else
+            {
+                gridControl1.DataSource = null;
+            }
+            
+        }
+
+        private void GridSetting()
+        {
+            for(int i = 0; i < gridView1.Columns.Count; i++)
+            {
+                gridView1.Columns[i].AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+                gridView1.Columns[i].AppearanceHeader.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
+            }
+        }
+
+        private void gridView1_RowCellStyle(object sender, DevExpress.XtraGrid.Views.Grid.RowCellStyleEventArgs e)
+        {
+            for (int i = 0; i < gridView1.Columns.Count; i++)
+            {
+                gridView1.Columns[i].AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+                gridView1.Columns[i].AppearanceHeader.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
+            }
+            if (e.Column.FieldName == "Barcode")
+            {
+                e.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            }
+            if(e.Column.FieldName == "PT")
+            {
+                e.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
+            }
+        }
+    }
+}
